543921c78fb7a46aaca06d21250dde66f6ffaab1
Pawel <pawelt140@gmail.com>
2022-05-26 11:00:35 +0200

    First commit

---
+++ Projekt_test/Projekt_test/main.c
@@ -0,0 +1,18 @@
+/*
+ * Projekt_test.c
+ *
+ * Created: 22.05.2022 11:36:47
+ * Author : 101
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
